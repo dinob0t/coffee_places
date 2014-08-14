8dd68a1c87f1f9c2346a0fd2dcb26d4e4281c883
Georgi K <georgiknox@gmail.com>
2014-08-14 19:10:28 -0400

    fixed script to use zipped lists

--- async_suns.py
+++ async_suns.py
@@ -31,8 +31,11 @@ def generate_url_list(base_url, year):
   return url_list, file_list
 
 @asyncio.coroutine
-def save_to_file(img, idx, file_list):
-  file_name = file_list[idx]
+def save_to_file(img, file_name):
+  # file_idx = idx + 10;
+  # file_name = file_list[file_idx]
+  print(type(file_name))
+  print("printing file name: " + file_name)
   f = open('./2011/' + file_name,'wb')
   f.write(img)
   f.close()
@@ -42,7 +45,7 @@ def fetch(url):
   data = ""
   try:
     yield from asyncio.sleep(1)
-    response = yield from aiohttp.request('GET', url, connector=connector)
+    response = yield from aiohttp.request('GET', url)
   except Exception as exc:
       print("ERROR ", url, 'has error', repr(str(exc)))
   else:
@@ -53,42 +56,38 @@ def fetch(url):
   return data
 
 @asyncio.coroutine
-def fetch_image(url, idx, file_list):
-    print("fetching image with idx %d", idx)
-    page = yield from fetch(url)
-    print("got image with idx %d", idx)
+def fetch_image(url, file_name):
+    img = yield from fetch(url)
 
     # save image to file
-    yield from save_to_file(page, idx, file_list)
+    yield from save_to_file(img, file_name)
 
 @asyncio.coroutine
-def process_batch_of_urls(round, urls, file_list):
-  print("Batch %d starting" % round)
+def process_batch_of_urls(urls, file_names):
   coros = []
-  for idx, url in enumerate(urls):
-    coros.append(asyncio.Task(fetch_image(url, idx, file_list)))
+  for url, file_name in zip(urls, file_names):
+    coros.append(asyncio.Task(fetch_image(url, file_name)))
 
   yield from asyncio.gather(*coros)
-  print("Round %d finished" % round)
 
 @asyncio.coroutine
 def process_all():
   print('### Started ###')
   start_time = time.time()
-  urls, file_list = generate_url_list('http://jsoc.stanford.edu/data/hmi/images/', 2011)
+  url_list, file_list = generate_url_list('http://jsoc.stanford.edu/data/hmi/images/', 2011)
 
-  chunks=[urls[x:x+100] for x in range(0, len(urls), 100)]
-  print("Total number of batches %d" % len(chunks))
+  url_chunks=[url_list[x:x+100] for x in range(0, len(url_list), 100)]
+  file_chunks=[file_list[x:x+100] for x in range(0, len(file_list), 100)]
 
-  for idx, batch in enumerate(chunks):
-    yield from process_batch_of_urls(idx, batch, file_list)
+  for urls, file_names in zip(url_chunks, file_chunks):
+    yield from process_batch_of_urls(urls, file_names)
 
   total_time = time.time() - start_time
-  print("Total number of api requests %d" % len(urls))
+  print("Total number of api requests %d" % len(url_list))
   print("Total time taken to process api requests is %s seconds" % total_time)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     # connector stores cookies between requests and uses connection pool
-    connector = aiohttp.TCPConnector(share_cookies=True, loop=loop)
+    # connector = aiohttp.TCPConnector(share_cookies=True, loop=loop)
     loop.run_until_complete(process_all())
